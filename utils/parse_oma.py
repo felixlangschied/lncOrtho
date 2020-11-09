07853ed71cd074e775da2e4b23b0dcfa92207558
felixlangschied <felix_langschied@web.de>
2020-11-09 17:28:32 +0100

    first working version of the oma_parser

--- utils/parse_oma.py
+++ utils/parse_oma.py
@@ -1,16 +1,12 @@
-#c_gtf = '/share/project/felixl/ncOrtho/data/mouse_ref_core/core_annotation'
-c_gtf = '/home/felixl/Desktop/tmp/gft_input.txt'
-ens2oma_map = '/share/project/felixl/ncOrtho/data/oma/oma-ensembl.txt'
-oma_groups = '/share/project/felixl/ncOrtho/data/oma/oma-groups.txt'
-r_gtf = '/share/project/felixl/ncOrtho/data/mouse_ref_core/reference_data/Mus_musculus.GRCm38.101.gtf'
 
 import os
 import glob
-import multiprocessing as mp
-import numpy as np
+import re
 
-from oma_lib.ensembl2oma import findOMAprefix_fromEnsemblGTF
-from oma_lib.ensembl2oma import map_ensembl2OMA
+from utils.oma_lib.ensembl2oma import findOMAprefix_fromEnsemblGTF
+from utils.oma_lib.ensembl2oma import map_ensembl2OMA
+from utils.oma_lib.ensembl2oma import find_core_ortholog
+from utils.oma_lib.ensembl2oma import map_OMA2ensembl
 
 def oma_parser(r_gtf, c_gtf, ens2oma_map, oma_groups):
     # parse gtfs
@@ -51,26 +47,25 @@ def oma_parser(r_gtf, c_gtf, ens2oma_map, oma_groups):
         print('No valid GTF file (<.gtf>) found at\n'
               '{}'.format(r_gtf))
 
-
+    # find ID prefix of taxa in the OMA database
     core_oma_ids = []
-    no_oma_id = []
+    no_oma_taxon = []
     for core in core_paths:
         core_oma = findOMAprefix_fromEnsemblGTF(core, ens2oma_map)
         if core_oma == None:
-            no_oma_id.append(core)
+            no_oma_taxon.append(core)
         else:
             core_oma_ids.append(core_oma)
     ref_oma = findOMAprefix_fromEnsemblGTF(ref_path, ens2oma_map)
 
-    # Body
-    if no_oma_id:
+    if no_oma_taxon:
         print('No OMA id found for:'
-              '{}\n'.format('\n'.join(no_oma_id)))
+              '{}\n'.format('\n'.join(no_oma_taxon)))
         print('Starting to map reference Proteins to OMA orthologs of the remaining core species')
     else:
         print('\nStarting to extract all ensembl gene ids from the reference species')
 
-    # Extract ensembl gene ids from reference GTF
+    # Extract ensembl gene ids from the reference GTF
     ensembl_ids = []
     with open(r_gtf, 'r') as r_file:
         for line in r_file:
@@ -84,62 +79,70 @@ def oma_parser(r_gtf, c_gtf, ens2oma_map, oma_groups):
 
     # convert list to set to get rid of duplicates
     ensembl_ids = set(ensembl_ids)
-    print('\nFound {} unique ensembl gene ids (Type: CDS) in {}'.format(len(ensembl_ids), r_gtf.split('/')[-1]))
-    print('\nStarting to map ensembl gene ids to the OMA ids. This might take a while')
+    print('\nFound {} unique ensembl gene ids (Type: CDS) in {}'
+          .format(len(ensembl_ids), r_gtf.split('/')[-1]))
+    print('\nStarting to map ensembl gene ids of the reference species to the respective OMA ids')
 
-    #map list of ensembl gene ids in the reference species to the OMA id
+    # map ensembl ids of the reference species to the OMA ids
+    map_dict, no_oma = map_ensembl2OMA(ensembl_ids, ens2oma_map)
+    if no_oma:
+        print('Could not find oma ids for {} ensembl ids\n'.format(len(no_oma)))
+    del ensembl_ids
 
-    # maybe implement multiprocessing
-    # output = mp.Queue()
-    # num_workers = 3
-    # chunks = np.array_split(list(ensembl_ids), num_workers)
-    # processes = [mp.Process(target=map_ensembl2OMA, args=(chunks[x], ens2oma_map, output)) for x in range(num_workers)]
-    # for p in processes:
-    #     p.start()
-    # for p in processes:
-    #     p.join()
-    # results = [output.get() for p in processes]
-    # print(results)
+    #print(map_dict)
 
-    map_dict = map_ensembl2OMA(ensembl_ids, ens2oma_map)
+    print('Starting to map reference OMA ids to the OMA groups\n')
 
-    output = []
-    with open('ensembl2oma.txt','w') as file:
-        for key, value in map_dict.items():
-            line = key + '\t' + value
-            output.append(line)
-            file.write('\n'.join(output))
-
-
-
+    # load dictionary with reference OMAid as key and all other OMAids
+    # of the orthologous group as the values
+    ref_oma2rest_oma = {}
+    with open(oma_groups, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if ref_oma in line:
+                hit = re.search(ref_oma + '\d+', line)
+                hit = hit.group()
+                line_list = line.split('\t')[1:]
+                line_list.remove(hit)
+                ref_oma2rest_oma[hit] = line_list
 
 
-    #print(list(map_dict.keys())[0:10])
-    #print('Did not find orthologs for {} ensembl ids'.format(len(not_found)))
+    taxon_suff = core_oma_ids[0]
+    print('Starting to extract orthologs from the OMA groups for:\n'
+          '## {} ##'.format(taxon_suff))
+    core_oma2ref_ens = find_core_ortholog(map_dict, ref_oma2rest_oma, taxon_suff)
+
+    # map OMA ids of the core species back to the ensembl IDs
+    core_omas = core_oma2ref_ens.keys()
+    core_map = map_OMA2ensembl(core_omas, ens2oma_map)
+
+    final_dict = {}
+    for oma_id in core_oma2ref_ens:
+        key = core_oma2ref_ens[oma_id]
+        value = core_map[oma_id]
+        final_dict[key] = value
+
+    #write output
+
+
+    #print(final_dict)
+    print(len(final_dict))
 
 
 
 
 
 
-    print(core_oma_ids)
-    print(no_oma_id)
-    print(ref_oma)
 
+
+# testing
+
+#c_gtf = '/share/project/felixl/ncOrtho/data/mouse_ref_core/core_annotation'
+c_gtf = '/home/felixl/Desktop/tmp/gft_input.txt'
+ens2oma_map = '/share/project/felixl/ncOrtho/data/oma/oma-ensembl.txt'
+oma_groups = '/share/project/felixl/ncOrtho/data/oma/oma-groups.txt'
+r_gtf = '/share/project/felixl/ncOrtho/data/mouse_ref_core/reference_data/Mus_musculus.GRCm38.101.gtf'
 
 oma_parser(r_gtf, c_gtf, ens2oma_map, oma_groups)
 
 
-#
-# print('this is ref_oma')
-# print(ref_oma)
-# print('this is core_ome')
-# print(core_oma)
-# for value in core_oma:
-#     print(value)
-#
-#
-#
-# if core_oma:
-#     print('Continuing to build files of OMA pairwise orthologs between\n'
-#           'the reference species and each core species represented in the OMA database')
